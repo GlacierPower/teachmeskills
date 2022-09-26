e1444ea09985330bf2b73c56f6d00b16f400cd77
Julia <onlymirage@live.ru>
2022-09-26 22:09:16 +0300

    Fourth commite

--- app/src/main/java/com/example/lesson4/MainActivity.java
+++ app/src/main/java/com/example/lesson4/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("");
-        System
+        for (int i=0;i<9;i++){
+
+        }
     }
 }
