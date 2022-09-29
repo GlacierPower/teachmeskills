4ff20c4a43357205ff556094f139adf3c4e701e1
neformatic <reznik.workspace@gmail.com>
2022-09-29 20:10:21 +0300

    added message

--- app/src/main/java/com/example/lesson4/MainActivity.java
+++ app/src/main/java/com/example/lesson4/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("");
-        for (int i=0;i<9;i++){
+        System.out.println("Conflicts");
 
         }
     }
