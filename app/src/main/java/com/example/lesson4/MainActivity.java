aedd8df7eba683af7ebc3e075041d80fc1ace094
neformatic <reznik.workspace@gmail.com>
2022-09-29 20:13:13 +0300

    added test 2 confkict

--- app/src/main/java/com/example/lesson4/MainActivity.java
+++ app/src/main/java/com/example/lesson4/MainActivity.java
@@ -10,9 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("");
-        for (int i=0;i<9;i++){
 
+        System.out.println("warning");
         }
     }
 }
